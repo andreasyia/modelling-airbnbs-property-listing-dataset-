6fc5095eccdcdcb0e5aefec749f9768d93211b3b
Andreas  Yianni <andreasyianni613@gmail.com>
2023-08-26 17:00:36 +0100

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# modelling-airbnbs-property-listing-dataset-
+# **Modelling-Airbnbs-Property-Listing-Dataset**
+# Table of Contents
+1. [Introduction](#introduction)
+2. [Data Preperation](#section-1)
+    - [Subsection 1.1](#subsection-1.1)
+    - [Subsection 1.2](#subsection-1.2)
+
+
+## Introduction
+The devised framework for model development systematically addresses various tasks undertaken by the Airbnb team. Beginning with precise task definitions and data collection, the framework encompasses feature engineering and preprocessing, catering to tabular, text, and image data. Model architectures are thoughtfully selected, followed by rigorous hyperparameter tuning to optimize performance. Models are trained with careful consideration of overfitting prevention, and validation metrics inform the choice of model architecture. Ensemble techniques are explored for added robustness. Interpretability tools shed light on model predictions, aiding decision-making. Final models are rigorously evaluated against test data before deployment, complemented by continuous monitoring for maintenance. Comprehensive documentation and effective communication with stakeholders facilitate collaboration. This holistic approach ensures the deployment of effective, interpretable, and adaptable models aligned with Airbnb's data-driven practices.
+
+## Data Preperation
+We're working with Airbnb tabular data to create a comprehensive processing framework. To handle the data, we begin by creating a tabular_data.py script. We define the remove_rows_with_missing_ratings function to eliminate rows with missing rating values, enhancing data quality. For the "Description" column, we implement combine_description_strings to join list items into coherent strings, eliminating empty quotes and the "About this space" prefix. Next, the set_default_feature_values function fills empty "guests", "beds", "bathrooms", and "bedrooms" entries with 1, preserving the data's integrity. All these functions are integrated into clean_tabular_data, a comprehensive function that takes raw data as input and applies these steps sequentially. Finally, within an if __name__ == "__main__" block, we load the raw data, call clean_tabular_data on it, and save the processed data as clean_tabular_data.csv, maintaining the file structure."
+
+### Subsection 1.1
+Content here...
+
+### Subsection 1.2
+Content here...
+
---
+++ tabular_data.py
@@ -0,0 +1,138 @@
+import ast
+import os
+import pandas as pd
+
+def remove_rows_with_missing_ratings(df):
+    """
+    Remove rows with missing rating values from the DataFrame.
+
+    Parameters:
+            df (pandas.DataFrame): The input DataFrame containing rating columns.
+
+    Returns:
+            pandas.DataFrame: A DataFrame with rows containing missing rating values removed.
+    """
+    columns_for_cleaning = ['Cleanliness_rating', 
+                            'Accuracy_rating', 
+                            'Location_rating', 
+                            'Check-in_rating', 
+                            'Value_rating', 
+                            'Communication_rating']
+    df_cleaned = df.dropna(subset=columns_for_cleaning)
+
+    return df_cleaned 
+
+def preprocess_description(description):
+    """
+    This function takes a string representation of a list containing descriptions, it then processes and combines them and 
+    removing empty strings and a prefix.
+
+    Parameters:
+            description(str):  A string containing a list-like representation of descriptions.
+
+    Returns:
+            combined_description(str): The combined and cleaned description.
+    """
+    try:
+        # Convert string to a list
+        description_list = ast.literal_eval(description)
+        # Remove empty strings
+        description_list = [item for item in description_list if item.strip() != '']
+        # Remove prefix and extra whitespace
+        description_list = [item.replace("About this space", "").strip() for item in description_list]
+        # Join cleaned descriptions with a space
+        combined_description = " ".join(description_list)
+
+        return combined_description
+    
+    except (ValueError, SyntaxError):
+        return None
+
+def combine_description_strings(df):
+    """
+    This function applies the 'preprocess_description' function to the 'Description'
+    column of the given DataFrame, removing rows with missing descriptions.
+
+    Parameters:
+            df(pandas.DataFrame): The DataFrame containing the 'Description' column.
+        
+    Returns:
+            df(pandas.DataFrame): The modified DataFrame with cleaned and combined descriptions.
+    """
+    df['Description'] = df['Description'].apply(preprocess_description)
+    df = df.dropna(subset=['Description'])
+
+    return df
+
+def set_default_feature_values(df):
+    """    
+    This function takes a DataFrame and replaces empty values in the columns
+    "guests", "beds", "bathrooms", and "bedrooms" with the number 1.
+    
+    Parameters:
+        df(pandas.DataFrame): The DataFrame containing the specified columns.
+        
+    Returns:
+        df(pandas.DataFrame): The modified DataFrame with replaced values.
+    """
+    columns_for_cleaning = ['guests', 
+                            'beds', 
+                            'bathrooms', 
+                            'bedrooms' 
+                            ]
+    for column in columns_for_cleaning:
+        df[column].fillna(1, inplace=True)
+
+    return df
+
+def clean_tabular_data(dataframe):
+    """    
+    This function takes a raw DataFrame and applies sequential processing steps
+    to clean and modify the data.
+    
+    Parameters:
+        dataframe(pandas.DataFrame): The raw DataFrame to be processed.
+        
+    Returns:
+        df_proceessed(pandas.DataFrame): The processed DataFrame after all cleaning steps.
+    """
+    df_cleaned_ratings = remove_rows_with_missing_ratings(dataframe)
+    df_cleaned_descriptions = combine_description_strings(df_cleaned_ratings)
+    df_processed = set_default_feature_values(df_cleaned_descriptions)
+
+    return df_processed  
+
+def load_airbnb(file_path, label):
+    """
+    Load features and labels from the Airbnb tabular data.
+
+    Parameters:
+        file_path(str): Path to the CSV file containing the tabular data.
+        label(str): Name of the column to be used as the label.
+
+    Returns:
+        tuple: A tuple containing a pandas DataFrame of features and a pandas Series of labels.
+    """
+    data = pd.read_csv(file_path)
+    features = data.drop(columns=[label])
+    labels = data[label]
+
+    return features, labels
+
+if __name__ == "__main__":
+
+    csv_file_path = '/Users/andreasyianni/Desktop/Educational Training /Data Scientist Training/AiCore Training/Projects/Modelling-Airbnbs-Property-Listing-Dataset/airbnb-property-listings/tabular_data/listing.csv'
+    file_path = '/Users/andreasyianni/Desktop/Educational Training /Data Scientist Training/AiCore Training/Projects/Modelling-Airbnbs-Property-Listing-Dataset/airbnb-property-listings/tabular_data/clean_tabular_data.csv'
+
+    # Open the CSV file using a context manager and read it into a DataFrame
+    with open(csv_file_path, 'r') as file:
+        df = pd.read_csv(file)
+
+    processed_data = clean_tabular_data(df)
+    # Get the directory of the raw CSV file
+    directory = os.path.dirname(csv_file_path)
+    # Path to save the processed data CSV file
+    processed_csv_file_path = os.path.join(directory, 'clean_tabular_data.csv')
+    # Save the processed data as CSV
+    processed_data.to_csv(processed_csv_file_path, index=False)
+    print("Data processing and saving complete")
