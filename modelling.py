59673f758fa3cb5d2edc25a2550d86b7a4b9d2a6
Andreas  Yianni <andreasyianni613@gmail.com>
2023-09-08 16:17:40 +0100

    third commit

--- modelling.py
+++ modelling.py
@@ -1,16 +1,15 @@
-from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
-from sklearn.linear_model import SGDRegressor 
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.ensemble import GradientBoostingRegressor, GradientBoostingClassifier, RandomForestRegressor, RandomForestClassifier
+from sklearn.linear_model import SGDRegressor, LogisticRegression, SGDClassifier
+from sklearn.metrics import mean_squared_error, r2_score, f1_score, precision_score , recall_score, accuracy_score
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.svm import SVR
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
 from tabular_data import load_airbnb
 import itertools
 import joblib
 import json
 import numpy as np
 import os 
-import pandas as pd
 
 # Define the path to the data file
 file_path = '/Users/andreasyianni/Desktop/Educational Training /Data Scientist Training/AiCore Training/Projects/Modelling-Airbnbs-Property-Listing-Dataset/airbnb-property-listings/tabular_data/clean_tabular_data.csv'
@@ -115,8 +114,26 @@ def tune_regression_model_hyperparameters(model_class, X_train, y_train, X_val, 
 
     return best_estimator, best_params, performance_metrics
 
+def tune_classification_model_hyperparameters(model_class, X_train, y_train, X_val, y_val, hyperparameters):
+    
+    model = model_class
+    model.fit(X_train, y_train)
+    model.predict(X_val)
 
-def save_model(model, hyperparams, metrics, model_name, folder):
+    grid = GridSearchCV(model_class, param_grid=hyperparameters, refit = True, verbose = 3,n_jobs=-1)
+    grid.fit(X_train, y_train)
+
+    best_estimator = grid.best_estimator_
+    best_params = grid.best_params_
+
+    val_predictions = best_estimator.predict(X_val)
+    val_accuracy = accuracy_score(y_val, val_predictions)
+
+    performance_metrics = {"validation_accuracy": val_accuracy}
+
+    return best_estimator, best_params, performance_metrics
+
+def save_model(model, hyperparams, metrics, folder):
     """
     Save the best regression model, hyperparameters, and performance metrics to files.
 
@@ -127,6 +144,8 @@ def save_model(model, hyperparams, metrics, model_name, folder):
         - model_name (str): The name of the model.
         - folder (str): The folder where the files will be saved.
     """
+    # Get the model class name
+    model_name = model.__class__.__name__
 
     # Save the model to a joblib file
     model_path = os.path.join(folder, f"{model_name}_model.joblib")
@@ -142,7 +161,9 @@ def save_model(model, hyperparams, metrics, model_name, folder):
     with open(metrics_path, "w") as f:
         json.dump(metrics, f, indent=4)
 
-def evaluate_all_models(X_train, y_train, X_val, y_val, hyperparameters_list, models_list):
+def evaluate_all_models(X_train_reg, y_train_reg, X_val_reg, y_val_reg, regression_hyperparameters,
+                        X_train_cls, y_train_cls, X_val_cls, y_val_cls, classification_hyperparameters,
+                        regression_models, classification_models, task_folder):
     """
     This function performs hyperparameter tuning and evaluation for multiple regression models using the provided
     training and validation datasets. It iterates over a list of regression models and their corresponding
@@ -160,17 +181,17 @@ def evaluate_all_models(X_train, y_train, X_val, y_val, hyperparameters_list, mo
     Returns:
         None
     """
-# Iterate over the models and hyperparameters and perform tuning and evaluation
-    for model_config, model_class in zip(hyperparameters_list, models_list):
+# Iterate over the models and hyperparameters and perform tuning and evaluation for regression models
+    for model_config, model_class in zip(regression_hyperparameters, regression_models):
         model_name = model_config['model_name']
         hyperparameters_list = model_config['hyperparameters']
 
         best_estimator, best_params, performance_metrics = tune_regression_model_hyperparameters(
             model_class=model_class,
-            X_train=X_train,
-            y_train=y_train,
-            X_val=X_val,
-            y_val=y_val,
+            X_train=X_train_reg,
+            y_train=y_train_reg,
+            X_val=X_val_reg,
+            y_val=y_val_reg,
             hyperparameters=hyperparameters_list)
 
         print(f"Model: {model_name}")
@@ -179,9 +200,34 @@ def evaluate_all_models(X_train, y_train, X_val, y_val, hyperparameters_list, mo
         print("Performance_metrics:", performance_metrics)
 
         # Save the model, hyperparameters, and metrics
-        save_model(best_estimator, best_params, performance_metrics, model_name, folder="models/regression")
+        task_folder = "models/regression"
+        save_model(best_estimator, best_params, performance_metrics, task_folder)
+    
+    # Iterate over the models and hyperparameters and perform tuning and evaluation for regression models
+    for model_config, model_class in zip(classification_hyperparameters, classification_models):
+        model_name = model_config['model_name']
+        hyperparameters_list = model_config['hyperparameters']
 
-def find_best_model(X_train, y_train, X_val, y_val, hyperparameters_list, models_list):
+        best_estimator, best_params, performance_metrics = tune_classification_model_hyperparameters(
+            model_class=model_class,
+            X_train=X_train_cls,
+            y_train=y_train_cls,
+            X_val=X_val_cls,
+            y_val=y_val_cls,
+            hyperparameters=hyperparameters_list)
+
+        print(f"Model: {model_name}")
+        print("Best Estimator:", best_estimator)
+        print("Best Hyperparameters:", best_params)
+        print("Performance_metrics:", performance_metrics)
+
+        # Save the model, hyperparameters, and metrics
+        task_folder = "models/classification"
+        save_model(best_estimator, best_params, performance_metrics, task_folder)
+
+def find_best_model(X_train_reg, y_train_reg, X_val_reg, y_val_reg, regression_hyperparameters,
+                    X_train_cls, y_train_cls, X_val_cls, y_val_cls, classification_hyperparameters,
+                    regression_models, classification_models):
     """
     Find the best regression model among a list of models by evaluating their performance on validation data.
 
@@ -198,40 +244,71 @@ def find_best_model(X_train, y_train, X_val, y_val, hyperparameters_list, models
         - best_hyperparams (dict): The best hyperparameters.
         - performance_metrics (dict): Performance metrics on the validation data.
     """
-    best_model = None
-    best_hyperparams = None
-    best_performance_metrics = {"validation_RMSE": float("inf")}
+    best_model_reg = None
+    best_hyperparams_reg = None
+    best_performance_metrics_reg = {"validation_RMSE": float("inf")}
 
-    for model_config, model_class in zip(hyperparameters_list, models_list):
+    for model_config, model_class in zip(regression_hyperparameters, regression_models):
         model_name = model_config['model_name']
         hyperparameters_list = model_config['hyperparameters']
 
-        best_estimator, best_params, performance_metrics = tune_regression_model_hyperparameters(
+        best_estimator_reg, best_hyperparams_reg, performance_metrics_reg = tune_regression_model_hyperparameters(
             model_class=model_class,
-            X_train=X_train,
-            y_train=y_train,
-            X_val=X_val,
-            y_val=y_val,
+            X_train=X_train_reg,
+            y_train=y_train_reg,
+            X_val=X_val_reg,
+            y_val=y_val_reg,
             hyperparameters=hyperparameters_list)
 
         print(f"Model: {model_name}")
-        print("Best Estimator:", best_estimator)
-        print("Best Hyperparameters:", best_params)
-        print("Performance_metrics:", performance_metrics)
+        print("Best Estimator:", best_estimator_reg)
+        print("Best Hyperparameters:", best_hyperparams_reg)
+        print("Performance_metrics:", performance_metrics_reg)
 
-        if performance_metrics["validation_RMSE"] < best_performance_metrics["validation_RMSE"]:
-            best_model = best_estimator
-            best_hyperparams = best_params
-            best_performance_metrics = performance_metrics
+        if performance_metrics_reg["validation_RMSE"] < best_performance_metrics_reg["validation_RMSE"]:
+            best_model_reg = best_estimator_reg
+            best_hyperparams_reg = best_hyperparams_reg
+            best_performance_metrics_reg = performance_metrics_reg
 
-    return best_model, best_hyperparams, best_performance_metrics
+        # best_metrics_reg = best_model_reg, best_hyperparams, best_performance_metrics_reg
+    
+    best_model_cls = None
+    best_hyperparams_cls = None
+    best_performance_metrics_cls = {"validation_accuracy": 0}
+
+    for model_config, model_class in zip(classification_hyperparameters, classification_models):
+        model_name = model_config['model_name']
+        hyperparameters_list = model_config['hyperparameters']
+
+        best_estimator_cls, best_hyperparams_cls, performance_metrics_cls = tune_classification_model_hyperparameters(
+            model_class=model_class,
+            X_train=X_train_cls,
+            y_train=y_train_cls,
+            X_val=X_val_cls,
+            y_val=y_val_cls,
+            hyperparameters=hyperparameters_list)
+
+        print(f"Model: {model_name}")
+        print("Best Estimator:", best_estimator_cls)
+        print("Best Hyperparameters:", best_hyperparams_cls)
+        print("Performance_metrics:", performance_metrics_cls)
+
+        if performance_metrics_cls["validation_accuracy"] > best_performance_metrics_cls["validation_accuracy"]:
+            best_model_cls = best_estimator_cls
+            best_hyperparams_cls = best_hyperparams_cls
+            best_performance_metrics_cls = performance_metrics_cls
+
+        # best_metrics_cls = best_model_cls, best_hyperparams, best_performance_metrics_cls
+
+    return (best_model_reg, best_hyperparams_reg, best_performance_metrics_reg,
+            best_model_cls, best_hyperparams_cls, best_performance_metrics_cls)
 
 
 if __name__ == "__main__":
-    data = load_airbnb(file_path, "Price_Night")
-    X, y = data
-    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
 
+    data_price_night = load_airbnb(file_path, "Price_Night")
+    X_reg, y_reg = data_price_night
+    X_train_reg, X_val_reg, y_train_reg, y_val_reg = train_test_split(X_reg, y_reg, test_size=0.2, random_state=42)
 
     # Define hyperparameter grid
     grid = {
@@ -242,10 +319,10 @@ if __name__ == "__main__":
 
     best_model, best_hyperparams, performance_metrics = custom_tune_regression_model_hyperparameters(
         model_class=SGDRegressor,
-        X_train=X_train,
-        y_train=y_train,
-        X_val=X_val,
-        y_val=y_val,
+        X_train=X_train_reg,
+        y_train=y_train_reg,
+        X_val=X_val_reg,
+        y_val=y_val_reg,
         hyperparameters=grid
     )
     print("Best Model:", best_model)
@@ -253,7 +330,7 @@ if __name__ == "__main__":
     print("Performance Metrics:", performance_metrics)
 
     # Define a dictionary of hyperparameters for different regression models
-    hyperparameters_list = [
+    regression_hyperparameters = [
         {
             'model_name': 'GradientBoosting',
             'hyperparameters': {
@@ -299,14 +376,88 @@ if __name__ == "__main__":
         }
     ]
 
-    # Define a list of all the regression models
-    models_list = [GradientBoostingRegressor(), SVR(), DecisionTreeRegressor(), RandomForestRegressor(), SGDRegressor()]
-
-    evaluate_all_models(X_train, y_train, X_val, y_val, hyperparameters_list, models_list)
-
-    best_model, best_hyperparams, best_performance_metrics = find_best_model(X_train, y_train, X_val, y_val, hyperparameters_list, models_list)
     
-    # Print the best model, hyperparameters, and performance metrics
-    print("Best Model:", best_model)
-    print("Best Hyperparameters:", best_hyperparams)
-    print("Best Performance Metrics:", best_performance_metrics)
+
+   
+
+    # Load the Airbnb data and split it into features (X) and target (y)
+    data_category = load_airbnb(file_path, 'Category')
+    X_cls, y_cls = data_category
+
+    # Split the data into training and testing sets
+    X_train_cls, X_val_cls, y_train_cls, y_val_cls = train_test_split(X_cls, y_cls, test_size=0.2, random_state=42)
+
+    hyperparameters_sgd = {
+                      'loss': ['squared_error'],
+                      'penalty': ['elasticnet'],
+                      'max_iter': [1000, 2000],
+                      'alpha': [0.0001, 0.001]
+                       }
+
+    classification_hyperparameters = [
+    {
+        'model_name': 'GradientBoosting',
+        'hyperparameters': {
+            'loss': ['deviance'],
+            'n_estimators': [10, 100, 1000],
+            'criterion': ['squared_error', 'friedman_mse'],
+            'learning_rate': [0.1, 0.001, 0.0001]
+        }
+    },
+    {
+        'model_name': 'DecisionTree',
+        'hyperparameters': {
+            'criterion': ['gini', 'entropy'],
+            'max_depth': [None, 5, 10, 20],
+            'min_samples_split': [2, 5, 10]
+        }
+    },
+    {
+        'model_name': 'RandomForest',
+        'hyperparameters': {
+            'n_estimators': [10, 100, 150],
+            'max_depth': [None, 5, 10, 20],
+            'max_features': ['sqrt', 'log2'],
+            'min_samples_split': [2, 5, 10]
+        }
+    },
+    # {
+    #     'model_name': 'SGDClassifier',
+    #     'hyperparameters': {
+    #         'loss': ['log', 'modified_huber'],
+    #         'penalty': ['elasticnet'],
+    #         'max_iter': [8000, 10000],
+    #         'alpha': [0.0001, 0.001, 0.01],      
+    #         }
+    # }
+]
+
+# Define a list of all the regression models
+regression_models = [GradientBoostingRegressor(), SVR(), DecisionTreeRegressor(), RandomForestRegressor(), SGDRegressor()]
+
+# Define a list of all the classification models
+classification_models = [GradientBoostingClassifier(), DecisionTreeClassifier(), RandomForestClassifier(), SGDRegressor()]
+    
+# evaluate_all_models(X_train_cls, y_train_cls, X_val_cls, y_val_cls, classification_hyperparameters, regression_models, classification_models, task_folder)
+
+# best_model, best_hyperparams, best_performance_metrics = find_best_model(X_train, y_train, X_val, y_val, hyperparameters_regression, regression_models_list)
+
+(best_model_reg, best_hyperparams_reg, best_performance_metrics_reg, best_model_cls, best_hyperparams_cls, 
+best_performance_metrics_cls) = find_best_model(X_train_reg, y_train_reg, X_val_reg, 
+                                                    y_val_reg, regression_hyperparameters, 
+                                                    X_train_cls, y_train_cls, X_val_cls, 
+                                                    y_val_cls, classification_hyperparameters, 
+                                                    regression_models, classification_models)
+
+# Print the best model, hyperparameters, and performance metrics for regression models
+print("\nRegression Model\n")
+print("Best Model:", best_model_reg)
+print("Best Hyperparameters:", best_hyperparams_reg)
+print("Best Performance Metrics:", best_performance_metrics_reg)
+
+# Print the best model, hyperparameters, and performance metrics for classification models
+print("\nClassification Model\n")
+print("Best Model:", best_model_cls)
+print("Best Hyperparameters:", best_hyperparams_cls)
+print("Best Performance Metrics:", best_performance_metrics_cls)
+
